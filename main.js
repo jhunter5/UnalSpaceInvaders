0b9565c8bf141513213bc705f34ede9946384c20
jhunter5 <jhunter@unal.edu.co>
2024-03-03 12:40:48 -0500

    refactor propertie group in class invaders to array

--- main.js
+++ main.js
@@ -20,9 +20,9 @@ class Bullet{
 
     checkCollision(invaders) {
         invaders.forEach(invader => {
-          if (this.sprite.overlap(invader)) {
+          if (this.sprite.overlap(invader.sprite)) {
             invader.explosion();
-            invader.remove();
+            invader.sprite.remove();
             this.sprite.remove();
             bullets.splice(bullets.indexOf(this), 1);
           }
@@ -44,9 +44,9 @@ class Invader{
         let bullet = new Bullet(this.sprite.x, this.sprite.y, 'n');
         bullets.push(bullet);
     }
-    
+
     explosion(){
-        let explosion = new Sprite(this.sprite.x, this.sprite.y, 50, 50, 'd');
+        let explosion = new Sprite(this.sprite.x, this.sprite.y, 50, 50, 'n');
         explosion.img = './assets/invaderexplosion.gif';
         explosion.scale = .25;
         explosion.diameter = 50;
@@ -56,7 +56,7 @@ class Invader{
 
 class Invaders{
     constructor() {
-        this.group = new Group();
+        this.group = new Array();
         this.movement = 3;  
         this.repetition = 0;
     }
@@ -68,7 +68,7 @@ class Invaders{
         while (this.group.length < AMOUNT_OF_INVADERS) {
             x += 50;
             let invader = new Invader(x, y, 'd');
-            this.group.add(invader.sprite);
+            this.group.push(invader);
             i++;
             if (i % 10 == 0) {
                 x = 100;
@@ -83,19 +83,19 @@ class Invaders{
         }
         this.group.forEach(invader => {
             console.log(this.repetition);
-            invader.x += this.movement;
+            invader.sprite.x += this.movement;
         });
-        if (this.group[this.group.length - 1].x > WINDOW_WIDTH - 50) {
+        if (this.group[this.group.length - 1].sprite.x > WINDOW_WIDTH - 50) {
             this.movement = -3
             this.repetition += 1;
         }
-        if (this.group[0].x < 50) {
+        if (this.group[0].sprite.x < 50) {
             this.movement = 3
             this.repetition += 1;
         }
         if (this.repetition % 2 == 0 && this.repetition != 0) {
             this.group.forEach(invader => {
-                invader.y += 50;
+                invader.sprite.y += 50;
                 this.repetition = 0;
             });
         }
